1dcde24c5f36dbdcbaeff7fcbfb5824dce6687a7
ru77 <ruzz.uffic@gmail.com>
2020-01-07 20:46:57 +0100

    first commit

--- src/app/App.java
+++ src/app/App.java
@@ -14,43 +14,35 @@ public class App extends HttpServlet{
      */
     private static final long serialVersionUID = -3967314453512919811L;
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
         System.out.println("Hello Java 1");
-    }
+  }
 
-    private String mymsg;
-    public void init() throws ServletException
-    {
-       mymsg = "Http Servlet Demo";
-    }
+  private int hitCount;
 
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
-      res.setContentType("text/html");
-      PrintWriter out = res.getWriter();
-      String[] dizionario =
-  		{
-  			"Sinonimi: ampio,consistente,ingente. Contrari: esiguo,insufficente,irrisorio.",
-  			"Sinonimi: babbione,babbuasso,balordo. Contrari: acuto,furbo,intelligente.",
-  			"Sinonimi: buzzurro,bifolco,campagnolo. Contrari: galantuomo,persona per bene,signore.",
-  			"Sinoimi: ossequente,rispettoso (di),disciplinato. Contrari: disobediente,ribelle,indomito.",
-  			"Sinoimi: mancante,privo,vuoto. Contrari: carico,pieno,colmo",
-  		};
-      switch(req.getParameter("cerca")){
-        case "abbondante":
-          out.println("<p>" + dizionario[0] + "</p>");
-          break;
-        case "babbeo":
-          out.println("<p>" + dizionario[1] + "</p>");
-          break;
-        case "cafone":
-          out.println("<p>" + dizionario[2] + "</p>");
-          break;
-        case "ubbidiente":
-          out.println("<p>" + dizionario[3] + "</p>");
-          break;
-        case "vacuo":
-          out.println("<p>" + dizionario[4] + "</p>");
-          break;
-      }
-    }
+   public void init() { hitCount = 0; }
+
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+      // Set response content type
+      response.setContentType("text/html");
+
+      // This method executes whenever the servlet is hit
+      // increment hitCount
+      hitCount++;
+      PrintWriter out = response.getWriter();
+      String title = "contatore accessi servlet";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+      out.println(docType +
+         "<html>\n" +
+            "<head><title>" + title + "</title></head>\n" +
+            "<body bgcolor = \"#f0f0f0\">\n" +
+               "<h1 align = \"center\">" + title + "</h1>\n" +
+               "<h2 align = \"center\">" + hitCount + "</h2>\n" +
+            "</body>
+         </html>"
+      );
+   }
 }
